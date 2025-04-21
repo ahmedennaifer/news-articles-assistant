6e38c354b7f98b2b95aad940194f517c641445fd
AhmedEnnaifer <ahmedennaifer00@gmail.com>
2025-04-21 12:57:06 +0200

    feat: add db + docker

---
+++ src/api/database/db.py
@@ -0,0 +1,30 @@
+"method for getting a session, to query the db."
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+USER = os.environ.get("POSTGRES_USER", "test")
+PW = os.environ.get("POSTGRES_PASSWORD", "pwd")
+DB = os.environ.get("POSTGRES_DB", "db")
+PORT = os.environ.get("POSTGRES_PORT", "15432")  # Using the custom port
+
+CONN_STR = f"postgresql://{USER}:{PW}@localhost:{PORT}/{DB}"
+engine = create_engine(CONN_STR)
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
+
+
+def get_db():
+    """
+    get a database session.
+    """
+    db = SessionLocal()
+    try:
+        return db
+    except Exception as e:
+        db.close()
+        raise e
---
+++ src/api/database/sql/init.sql
@@ -0,0 +1,12 @@
+-- schema for the table
+
+CREATE TABLE IF NOT EXISTS articles (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR NOT NULL, 
+  category VARCHAR NOT NULL, 
+  content TEXT NOT NULL 
+);
+
+INSERT INTO articles (title, category, content) 
+SELECT 'test', 'data for pg', 'init'
+WHERE NOT EXISTS (SELECT 1 FROM ARTICLES LIMIT 1);
