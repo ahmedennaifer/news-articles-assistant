46dd85450fa427bdacadb67ec1ceb3860fae0b39
AhmedEnnaifer <ahmedennaifer00@gmail.com>
2025-04-22 15:20:00 +0200

    feat: add db tool and better prompt+ pylint

--- src/assistant/components/string_to_chat_message.py
+++ src/assistant/components/string_to_chat_message.py
@@ -8,6 +8,8 @@ from haystack.dataclasses import ChatMessage
 
 @component
 class StringToChatMessage:
+    """class for the custom component"""
+
     @component.output_types(messages=List[ChatMessage])
     def run(self, messages: str):
         """converts a string input into a `ChatMessage`format that is expected for agent messages"""
