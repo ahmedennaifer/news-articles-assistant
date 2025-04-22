15a1c2f7ccfe6c60429196a972f12ed957d86325
AhmedEnnaifer <ahmedennaifer00@gmail.com>
2025-04-22 20:33:11 +0200

    feat: pgvector integration

--- src/api/database/sql/init.sql
+++ src/api/database/sql/init.sql
@@ -1,12 +1,16 @@
 -- schema for the table
 
+
+CREATE EXTENSION IF NOT EXISTS vector;
+
 CREATE TABLE IF NOT EXISTS articles (
   id SERIAL PRIMARY KEY,
   title VARCHAR NOT NULL, 
   category VARCHAR NOT NULL, 
-  content TEXT NOT NULL 
+  content TEXT NOT NULL,
+  embedding vector(3)
 );
 
-INSERT INTO articles (title, category, content) 
-SELECT 'test', 'data for pg', 'init'
+INSERT INTO articles (title, category, content, embedding) 
+SELECT 'test', 'data for pg', 'init', '[1,2,3]'
 WHERE NOT EXISTS (SELECT 1 FROM ARTICLES LIMIT 1);
