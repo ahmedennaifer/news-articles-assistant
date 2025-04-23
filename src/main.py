ae377d24d7fdf0419e606a9eda5fa21c6c388be4
AhmedEnnaifer <ahmedennaifer00@gmail.com>
2025-04-23 14:24:48 +0200

    misc: better image + better output formatting

--- src/main.py
+++ src/main.py
@@ -15,7 +15,6 @@ from assistant.tools.db.read_from_db import read_from_db_tool
 from assistant.tools.dummy_tool import weather_tool
 from assistant.vectordb.db import get_doc_store
 
-# TODO : connect agent to tool
 
 RAG_PARAGRAPH = "A groundbreaking paper published in the Journal of Molecular Biology explores the intricate relationship between mitochondrial function and cellular aging. The researchers utilized advanced microscopy techniques to observe real-time changes in mitochondrial morphology as cells progress through their life cycle. Their findings suggest that specific proteins regulating mitochondrial fusion and fission play a crucial role in determining cellular lifespan, potentially offering new targets for age-related disease interventions. What makes this study particularly noteworthy is its novel approach to tracking individual mitochondria over extended periods, revealing previously unobserved patterns of deterioration that precede cellular senescence. The implications extend beyond basic research, pointing toward potential therapeutic strategies that could modify these pathways to promote cellular health and longevity in aging populations"
 
@@ -75,12 +74,16 @@ def main():
         )
         if "agent" in result and result["agent"]:
             print(
+                f"{GREEN}Query: {q}{RESET}\n",
                 f"{GREEN}Tool Assistant:{RESET}",
                 f"{result['agent']['messages'][-1]._content[0].text} \n",  # pylint: disable=protected-access
                 f"Tool used: {result['agent']['messages'][-2]._content[0].origin}",  # pylint: disable=protected-access
             )
         elif "rag_pipe" in result and result["rag_pipe"]:
-            print(f"{YELLOW}Rag Assistant:{RESET}", result["rag_pipe"]["replies"][0])
+            print(
+                f"{YELLOW}Query: {q}{RESET} \n",
+            )
+            print(f"{YELLOW}Rag Assistant:{RESET}\n", result["rag_pipe"]["replies"][0])
 
 
 if __name__ == "__main__":
