c824a0a58cc294d5b91a57051912119a99122a4a
AhmedEnnaifer <ahmedennaifer00@gmail.com>
2025-04-26 20:10:58 +0200

    feat: add CSVMetadataExtractor, for extracting metadata from CSVs, into a doc

---
+++ src/assistant/components/indexing_components/csv_metadata_extractor.py
@@ -0,0 +1,34 @@
+"""custom component that extracts info and populates metadata"""
+
+from haystack import component, Document
+from typing import List
+import pandas as pd
+
+
+@component
+class CSVMetadataExtractor:
+    """Component that takes a str path and returnes a list of `Document` with metadata"""
+
+    def __init__(self, delimiter=",", header="infer") -> None:
+        self.delimiter = delimiter
+        self.header = header
+        self.docs: List[Document] = []
+
+    def _extract_meta_from_row(self, row: pd.Series) -> Document:
+        """extract fields and store as meta
+        :param row: `pd.Series` returned from `df.iterrows`
+        returns"""
+        content = row["content"]
+        title = row["title"]
+        category = row["category"]
+        return Document(content=content, meta={"category": category, "title": title})  # pyright: ignore
+
+    @component.output_types(documents=List[Document])
+    def run(self, source: str):
+        """entry point of the component
+        :param source: path of the csv"""
+        df = pd.read_csv(source, delimiter=self.delimiter, header=self.header)  # pyright: ignore
+        for _, row in df.iterrows():
+            doc = self._extract_meta_from_row(row)
+            self.docs.append(doc)
+        return self.docs
---
+++ tests/test_haystack_components.py
@@ -0,0 +1,18 @@
+from src.assistant.components.indexing_components.csv_metadata_extractor import (
+    CSVMetadataExtractor,
+)
+
+
+def test_csv_metadat_extractor():
+    csv = CSVMetadataExtractor()
+    docs = csv.run("data/final.csv")
+    assert (
+        "Quarterly profits at US media giant TimeWarner jumped 76% to $1.13bn (£600m)"  # first sentence
+        in docs[0].content
+    )
+    assert (
+        "It intends to adjust the way it accounts for a deal with German music publisher Bertelsmann's purchase of a stake in AOL Europe, which it had reported as advertising revenue. It will now book the sale of its stake in AOL Europe as a loss on the value of that stake"  # last sentence
+        in docs[0].content
+    )
+    assert docs[0].meta["category"] == "business"
+    assert docs[0].meta["title"] == "Ad sales boost Time Warner profit"
