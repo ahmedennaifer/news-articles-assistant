4920a07dfdb6184cb661ce817e97095c205afafe
AhmedEnnaifer <ahmedennaifer00@gmail.com>
2025-06-19 20:17:34 +0200

    chore: simpler main

--- Dockerfile
+++ Dockerfile
@@ -24,7 +24,9 @@ ENV POSTGRES_USER=${POSTGRES_USER}
 ENV POSTGRES_PASSWORD=${POSTGRES_PASSWORD}
 ENV POSTGRES_DB=${POSTGRES_DB}
 EXPOSE 5432
+
 # CMD ["/bin/bash", "-c","PYTHONPATH=/app python src/assistant/evaluators/mrr_evaluator.py"]
+
 CMD ["/bin/bash", "-c","PYTHONPATH=/app python src/main.py"]
 
 
--- src/main.py
+++ src/main.py
@@ -5,11 +5,12 @@ from assistant.pipelines.main_pipeline import run_main_pipe
 
 def main() -> None:
     """testing"""
-
-    while True:
-        query = input("Ask your question").strip()
-        print("Thinking...")
+    query = ""
+    while query != "exit":
+        query = input("Ask your question\n > ")
+        print("Thinking...\n")
         run_main_pipe(queries=[query])
+    print("Exiting...")
 
 
 if __name__ == "__main__":
